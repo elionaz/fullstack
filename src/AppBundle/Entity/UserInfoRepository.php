2032d1c95dd5b1ff4e3a8ebc09a7beee7b92ec9f
rene.escobar03 <rene.escobar03@telusinternational.com>
2018-04-26 14:02:34 -0600

    Generate and download Csv - repository

---
+++ src/AppBundle/Entity/UserInfoRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+
+class UserInfoRepository extends EntityRepository
+{
+
+    public function generateCsv($oCustomer)
+    {
+        $response = new StreamedResponse();
+        $sFileName = 'export_'.date('Y_m_d_h_m_s').'.csv';
+        $response->setCallback(function() use($oCustomer){
+            $handle = fopen('php://output', 'w+');
+            fputcsv($handle, array('Nombre', 'CURP', 'RFC', 'Correo', 'LugarNacimiento', 'CodigoPostal'), ';');
+            foreach ($oCustomer as $customer) {
+                fputcsv($handle, array(
+                    $customer->getFullName(),
+                    $customer->getCurp(),
+                    $customer->getRfc(),
+                    $customer->getEmail(),
+                    $customer->getNamePlaceOfBirth(),
+                    $customer->getPostCode(), ), ';');
+            }
+            fclose($handle);
+        });
+        $response->setStatusCode(200);
+        $response->headers->set('Content-Type', 'text/csv; charset=utf-8');
+        $response->headers->set('Content-Disposition', "attachment; filename='{$sFileName}'");
+        return $response;
+    }
+
+}
